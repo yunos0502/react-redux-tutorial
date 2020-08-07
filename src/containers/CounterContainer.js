4954caeb5f0ad4bbde29b9333a37610c6ef98a0c
Park Gyu Ae <yunos0502@gmail.com>
2020-08-07 11:55:11 +0900

    finish react-redux-tutorial

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,33 +1,21 @@
-import React from "react";
-// import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => {
+    dispatch(increase());
+  }, [dispatch]);
+  const onDecrease = useCallback(() => {
+    dispatch(decrease());
+  }, [dispatch]);
+
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-// const mapStateToProps = (state) => ({
-//   number: state.counter.number,
-// });
-
-// const mapDispatchToProps = (dispatch) => ({
-//   // 임시 함수
-//   increase: () => {
-//     dispatch(increase());
-//   },
-//   decrease: () => {
-//     dispatch(decrease());
-//   },
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
-
-export default connect(
-  (state) => ({ number: state.counter.number }),
-  // (dispatch) => bindActionCreators({ increase, decrease }, dispatch)
-  { increase, decrease }
-)(CounterContainer);
+export default CounterContainer;
